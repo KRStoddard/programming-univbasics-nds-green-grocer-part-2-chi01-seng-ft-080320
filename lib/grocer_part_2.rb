c12323ec5f7d0c5c23efad810f48abd033667467
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-18 13:23:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,8 +3,31 @@ require_relative './part_1_solution.rb'
 def apply_coupons(cart, coupons)
  new_cart = []
  coupons.each do |coupon|
-   cart_item = find_item_by_name_in_collection(coupon[:item], cart)
-   if cart_item
+   cart.each do |cart_item|
+     if coupon[:item] == cart_item[:item]
+       if cart_item[:count] > coupon[:count]
+         new_item = cart_item
+         new_item[:count] = cart_item[:count] - coupon_item[:count]
+         new_coupon_item = {
+           :item => "#{coupon[:item]} W/COUPON",
+           :price => coupon[:price] / coupon[:count],
+           :clearance => cart_item[:clearance],
+           :count => coupon[:count]
+         }
+         new_item << new_cart
+         new_coupon_item << new_cart 
+       end
+     end
+   end
+ end
+ cart.each do |hash|
+   new_cart_item = find_item_by_name_in_collection(hash[:item], new_cart)
+   if !new_cart_item
+     new_cart << hash 
+   end
+ end
+ new_cart
+ end 
      
     
 
